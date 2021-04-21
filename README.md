7b333ce96cf8cc7d2ea6363356eca3ff12612fee
Julien Ledun <j.ledun@iosystems.fr>
2021-04-21 10:58:17 +0200

    add options
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- README.md
+++ README.md
@@ -8,6 +8,28 @@ Buffer description is set on the node itself.
 
 Please, checkout [io-buffer-worker](https://github.com/jledun/io-buffer-worker "io-buffer-worker at github") README to learn how to build a description file.
 
+## encode node options
+
+* formatter la sortie pour un noeud ModbusWrite
+
+Le noeud ModbusWrite du module node-red-contrib-modbus n'écrit pas un buffer mais un tableau de valeurs entières de 16 bits.
+
+Activer cette option formatte automatiquement le buffer calculé en un tableau de valeurs entières de 16 bits, évitant l'emploi d'une fonction intermédiaire de mise en forme.
+
+* Inverser les octets
+
+Lors de la mise en forme du buffer en un tableau de valeurs entières de 16 bits, cette option permet d'inverser les octets de poids fort et de poids faible, à adapter en fonction de l'automate qui héberge le serveur Modbus.
+
+## decode node options
+
+* Source du buffer
+
+Par défaut : msg.payload
+
+Il peut arriver que le noeud en entrée fournisse un objet dans lequel se trouve le buffer. Décrire alors dans ce champ dans quelle propriété de l'objet source se trouve le buffer attendu.
+
+
+
 Enjoy !
 
 Julien Ledun <j.ledun@iosystems.fr>
--- node-red-contrib-buffer-worker/node-red-contrib-buffer-worker.js
+++ node-red-contrib-buffer-worker/node-red-contrib-buffer-worker.js
@@ -2,6 +2,27 @@ const bufferWorker = require("io-buffer-worker");
 
 module.exports = (RED) => {
 
+  function bufferToArrayOfValue(buf, config) {
+    if (!buf) return [];
+    let tmp = [];
+    let i = 0;
+    let val1 = 0;
+    let val2 = 0;
+    const reverse = config.arrayLE || false;
+    do{
+      if (reverse) {
+        val1 = (i < buf.length) ? buf[i] : 0;
+        val2 = (i + 1 < buf.length) ? buf[i + 1] : 0;
+      }else{
+        val1 = (i + 1 < buf.length) ? buf[i + 1] : 0;
+        val2 = (i < buf.length) ? buf[i] : 0;
+      }
+      tmp.push(val1 * 0x100 + val2);
+      i = i + 2;
+    }while (i < buf.length);
+    return tmp;
+  }
+
   function encodeNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
@@ -9,10 +30,11 @@ module.exports = (RED) => {
 
       send = send || function() { node.send.apply(node, arguments); }
       const endianness = config.endianness || "BE";
+      const modbusWrite = config.valueArray || false;
 
       try{
         const result = await bufferWorker.encode(JSON.parse(config.description), msg.payload, {endianness: endianness});
-        msg.payload = result;
+        msg.payload = (modbusWrite) ? bufferToArrayOfValue(result, config) : result;
         send(msg);
         if (done) done();
 
@@ -34,9 +56,20 @@ module.exports = (RED) => {
 
       send = send || function() { node.send.apply(node, arguments); }
       const endianness = config.endianness || "BE";
+      const inputField = config.inputField || "payload";
+      const buffer = inputField.split(".").reduce((acc, st) => acc[st], msg);
+      if (typeof buffer === "undefined") {
+        const e = new Error(`Le champ msg.${inputField} n'existe pas.`);
+        if (done) {
+          done(e);
+        }else{
+          node.error(e, msg);
+        }
+        return;
+      }
 
       try{
-        const result = await bufferWorker.decode(JSON.parse(config.description), msg.payload, {endianness: endianness});
+        const result = await bufferWorker.decode(JSON.parse(config.description), buffer, {endianness: endianness});
         msg.payload = result;
         send(msg);
         if (done) done();
